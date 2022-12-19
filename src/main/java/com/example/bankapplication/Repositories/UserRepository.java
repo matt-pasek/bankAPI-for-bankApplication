fc8c61f708261fbcbb8c61ae887d96c0d8ebe893
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-12-19 23:37:01 +0100

    user login with token generation

--- src/main/java/com/example/bankapplication/Repositories/UserRepository.java
+++ src/main/java/com/example/bankapplication/Repositories/UserRepository.java
@@ -2,24 +2,26 @@ package com.example.bankapplication.Repositories;
 
 import com.example.bankapplication.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Random;
 
 @Repository
 public class UserRepository {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public User loginUser(String username, String password) {
-        return jdbcTemplate.queryForObject("select * from users where username = ? and password = ?", User.class, username, password);
-    }
-
+    private final Random random = new Random();
     public int addUser(User user) {
-        jdbcTemplate.update("insert into users(username,password,balance) values (?,?,0)",
+        return jdbcTemplate.update("insert into users(username,password,balance) values (?,?,1000)",
                 user.getUsername(),
                 user.getPassword()
         );
-        return 0;
     }
 
     public int deleteUserById(int id){
@@ -36,11 +38,43 @@ public class UserRepository {
                 user.getId()
         );
         if (amount < 0) {
-            jdbcTemplate.update("insert into history(user_id, total) values (?,?)",
+            return jdbcTemplate.update("insert into history(user_id, total) values (?,?)",
                     user.getId(),
                     -amount
             );
         }
         return 0;
+    }
+
+    public String login(String username, String password) {
+        var token = sha256(random.nextInt() + "hello java");
+        try {
+            var usr = getUserByData(username, password);
+            var res = jdbcTemplate.update("INSERT INTO sessions (token, user_id) VALUES (?, ?)", token, usr.getId());
+            return res == 1 ? token : "";
+        } catch (EmptyResultDataAccessException e) {
+            return "No user";
+        }
+    }
+
+    public User getUserByData(String username, String password) {
+        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE username=? AND password=?", BeanPropertyRowMapper.newInstance(User.class), username, password);
+    }
+
+    private static String sha256(final String base) {
+        try{
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] hash = digest.digest(base.getBytes(StandardCharsets.UTF_8));
+            final StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                final String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
     }
 }
