8188739ce3503822088137ea6bf9628aade322bd
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-01-08 22:13:00 +0100

    generate blik endpoint

--- src/main/java/com/example/bankapplication/Controlers/BlikController.java
+++ src/main/java/com/example/bankapplication/Controlers/BlikController.java
@@ -1,12 +1,8 @@
 package com.example.bankapplication.Controlers;
 
-import com.example.bankapplication.Models.User;
 import com.example.bankapplication.Repositories.BlikRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/blik")
@@ -14,18 +10,11 @@ public class BlikController {
     @Autowired
     BlikRepository blikRepository;
 
-    @GetMapping("genereate")
-    public String generateBlikCode(@RequestBody User user) {
-        return blikRepository.makeBlik(user);
+    @PostMapping("/generate")
+    public String generateBlikCode(@RequestBody String data) {
+        var userId = data.split("&")[0].substring(7);
+        var token = data.split("&")[1].substring(6);
+        return blikRepository.makeBlik(Integer.parseInt(userId), token);
     }
 
-    @GetMapping("expire")
-    public int deleteBlikCode(@RequestBody User user) {
-        return blikRepository.expireBlik(user);
-    }
-
-    @GetMapping("validate")
-    public int validateBlikCode(@RequestBody String code, User user) {
-        return blikRepository.validateBlik(code, user);
-    }
 }
