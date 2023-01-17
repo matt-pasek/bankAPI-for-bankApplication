ce662bffee01deb289a3d282046802151b4535ef
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-01-17 13:43:33 +0100

    blik repository make and use blik

--- src/main/java/com/example/bankapplication/Repositories/BlikRepository.java
+++ src/main/java/com/example/bankapplication/Repositories/BlikRepository.java
@@ -2,45 +2,74 @@ package com.example.bankapplication.Repositories;
 
 import com.example.bankapplication.Models.Blik;
 import com.example.bankapplication.Models.Session;
+import com.example.bankapplication.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.Objects;
 
 @Repository
 public class BlikRepository {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public String makeBlik(int userId, String token) {
-        Blik blik = new Blik(userId);
-
-        var session = jdbcTemplate.query("select * from sessions where token = ?", BeanPropertyRowMapper.newInstance(Session.class), token);
-        if (session.get(0).getUser_id() != userId)
+    public String makeBlik(String token) {
+        var session = getSessionToken(token);
+        if (session == null)
             return "error";
 
+        String blikcode;
+        do {
+            blikcode = generateBlik();
+        } while (getBlik(blikcode) != null);
+
         jdbcTemplate.update("insert into blikcodes(userId, blik) values (?,?)",
-                blik.getUserId(),
-                blik.getBlik(),
+                session.getUser_id(),
+                blikcode,
                 false
         );
-        return blik.getBlik();
+        return blikcode;
     }
 
-    // TODO add targetId field in blikcodes to know to whom give money to
-    public int useBlik(int targetId, String code) {
-        if (jdbcTemplate.queryForObject("select count(*) from blik where blik = ?", Integer.class, code) == 0)
+    private String generateBlik() {
+        StringBuilder blik = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            blik.append((int) (Math.random() * 10));
+        }
+        return blik.toString();
+    }
+
+    public int useBlik(int amount, String code, String token) {
+        var session = getSessionToken(token);
+        if (session == null)
             return 0;
 
-        return jdbcTemplate.update("update blikcodes SET isUsed = true WHERE blik=?", code);
+        var blik = getBlik(code);
+        if (blik == null || blik.getTargetId() != null)
+            return 0;
+
+        if(getUser(blik.getUserId()).getBalance() < amount)
+            return 0;
+
+        return jdbcTemplate.update("update blikcodes SET targetId=?, amount=? WHERE blik=?", session.getUser_id(), amount, code);
     }
 
-    public int confirmBlik(String code, String token) {
-        var blikObj = jdbcTemplate.queryForObject("select * from blikcodes where blik=?", Blik.class, code);
+    private Blik getBlik(String code)  {
+        try {
+            return jdbcTemplate.queryForObject("select * from blikcodes where blik = ?", BeanPropertyRowMapper.newInstance(Blik.class), code);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
 
-        if(jdbcTemplate.queryForObject("select * from sessions where blik=?", Session.class, code).getUser_id() == blikObj.getUserId())
-            return jdbcTemplate.update("delete from blikcodes where blik=?", code) == 1 && jdbcTemplate.update("update users set balance=balance-100 where id=?", blikObj.getUserId()) == 1 ? 1 : 0;
-        else
-            return -1;
+    private Session getSessionToken(String token) {
+        return jdbcTemplate.queryForObject("select * from sessions where token = ?", BeanPropertyRowMapper.newInstance(Session.class), token);
+    }
+
+    private User getUser(int userId) {
+        return jdbcTemplate.queryForObject("select * from users where id = ?", BeanPropertyRowMapper.newInstance(User.class), userId);
     }
 }
