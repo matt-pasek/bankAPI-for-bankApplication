b9ff842d659d25aeaa45c02ff4d04ff7ff6439f5
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-01-17 13:44:58 +0100

    Update Blik.java
    removed generate blik, added target id and amount

--- src/main/java/com/example/bankapplication/Models/Blik.java
+++ src/main/java/com/example/bankapplication/Models/Blik.java
@@ -7,26 +7,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-
 public class Blik {
     private Integer id;
     private String blik;
     private int userId;
-    private boolean isUsed;
-
-    // on create blik generate code
-    // constructor
-    public Blik(int userId) {
-        this.userId = userId;
-        this.blik = generateBlik();
-        this.isUsed = false;
-    }
-
-    private String generateBlik() {
-        StringBuilder blik = new StringBuilder();
-        for (int i = 0; i < 6; i++) {
-            blik.append((int) (Math.random() * 10));
-        }
-        return blik.toString();
-    }
+    private Integer targetId;
+    private int amount;
 }
