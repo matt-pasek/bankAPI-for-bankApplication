644e0ed044c8d7c01e5c9c75d4987b6e2a15ce75
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-01-25 18:22:46 +0100

    get balance endpoint, removed redundant endpoints

--- src/main/java/com/example/bankapplication/Controlers/UserController.java
+++ src/main/java/com/example/bankapplication/Controlers/UserController.java
@@ -1,34 +1,26 @@
 package com.example.bankapplication.Controlers;
 
 import com.example.bankapplication.Models.LoginCredentials;
-import com.example.bankapplication.Models.User;
+import com.example.bankapplication.Models.Token;
 import com.example.bankapplication.Repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
     @PostMapping("/login")
     public String loginUser(@RequestBody LoginCredentials loginCredentials) {
         return userRepository.login(loginCredentials.getUsername(), loginCredentials.getPassword());
     }
 
-    @PostMapping("/add")
-    public int addUser(@RequestBody User user) {
-        return userRepository.addUser(user);
-    }
-
-    @PostMapping("/balance")
-    public int updateBalance(@RequestBody User user, int amount) {
-        return userRepository.updateBalance(user, amount);
-    }
-
-    @DeleteMapping("/delete")
-    public int deleteUserById(@RequestBody int id) {
-        return userRepository.deleteUserById(id);
+    @GetMapping("/balance")
+    public int getBalance(@RequestParam String username, @RequestHeader Token token) {
+        return userRepository.getBalance(username, token.getToken());
     }
 }
