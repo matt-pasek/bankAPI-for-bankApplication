4dcd927ce84ed51cf96e396760cb31917447101a
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-01-25 18:22:14 +0100

    configured data source and driver

--- src/main/java/com/example/bankapplication/BankApplication.java
+++ src/main/java/com/example/bankapplication/BankApplication.java
@@ -2,6 +2,11 @@ package com.example.bankapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class BankApplication {
@@ -10,4 +15,18 @@ public class BankApplication {
         SpringApplication.run(BankApplication.class, args);
     }
 
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("org.mariadb.jdbc.Driver");
+        ds.setUrl("jdbc:mariadb://localhost:3306/bankapi");
+        ds.setUsername("root");
+        ds.setPassword("");
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
 }
